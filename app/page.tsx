cee22742f18a8c494b2cd252b14925b3204cc4c3
Che' J. Holloway <chejholloway@outlook.com>
2024-01-20 22:26:30 -0800

    build(global): :bug: trying to build in order to deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
 "use client"
 import DashBoardBlock from "@/components/Dashboard/SCGDashboard";
 import { TradingViewProvider } from "../utils/TradingViewContext";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "SCG Dashboard | NextJS, Tailwinds",
-  description: "Demo page for SCG ASSET MANAGEMENT"
-};
 
 export default function Home() {
   return (
